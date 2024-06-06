adecd49c64b6eea2e732db1eec822bfba7b6478b
LuscaKF <lucasfernandesf18@gmail.com>
2024-06-05 23:04:50 -0300

    SRP - Single Responsibility Principle (Princípio da responsabilidade única)

---
+++ src/srp/main.ts
@@ -0,0 +1,20 @@
+import { ShoppingCart } from './shopping-cart';
+import { Order } from './order';
+import { Messaging } from './messaging';
+import { Persistency } from './persistency';
+import { Product } from './product';
+
+const shoppingCart = new ShoppingCart();
+const messaging = new Messaging();
+const persistency = new Persistency();
+const order = new Order(shoppingCart, messaging, persistency);
+
+shoppingCart.addItem(new Product('Camiseta', 49.9));
+shoppingCart.addItem(new Product('Caderno', 9.9));
+shoppingCart.addItem(new Product('Lápis', 1.59));
+
+console.log(order.orderStatus);
+console.log(shoppingCart.items);
+console.log(shoppingCart.total());
+order.checkout();
+console.log(order.orderStatus);
--- src/srp/shopping-cart.ts
+++ src/srp/shopping-cart.ts
@@ -1,9 +1,7 @@
-type CartItem = { name: string; price: number };
-type OrderStatus = 'open' | 'closed';
+import { CartItem } from './interfaces/cart-item';
 
 export class ShoppingCart {
   private readonly _items: CartItem[] = [];
-  private _orderStatus: OrderStatus = 'open';
 
   addItem(item: CartItem): void {
     this._items.push(item);
@@ -17,38 +15,14 @@ export class ShoppingCart {
     return this._items;
   }
 
-  get orderStatus(): OrderStatus {
-    return this._orderStatus;
-  }
-
   total(): number {
     return +this._items
       .reduce((total, next) => total + next.price, 0)
       .toFixed(2);
   }
 
-  checkout(): void {
-    if (this.isEmpty()) {
-      console.log('Seu carrinho está vazio!');
-      return;
-    }
-
-    this._orderStatus = 'closed';
-    this.sendMessage(`Seu pedido com total de ${this.total()} foi recebido!`);
-    this.saveOrder();
-    this.clear();
-  }
-
   isEmpty(): boolean {
     return this._items.length === 0;
-  }
-
-  sendMessage(msg: string): void {
-    console.log('Mesagem enviada:', msg);
-  }
-
-  saveOrder(): void {
-    console.log('Pedido salvo com sucesso...');
   }
 
   clear(): void {
@@ -56,14 +30,3 @@ export class ShoppingCart {
     this._items.length = 0;
   }
 }
-
-const shoppingCart = new ShoppingCart();
-shoppingCart.addItem({ name: 'Camiseta', price: 49.9 });
-shoppingCart.addItem({ name: 'Caderno', price: 9.9 });
-shoppingCart.addItem({ name: 'Lápis', price: 1.59 });
-
-console.log(shoppingCart.orderStatus);
-console.log(shoppingCart.items);
-console.log(shoppingCart.total());
-shoppingCart.checkout();
-console.log(shoppingCart.orderStatus);
